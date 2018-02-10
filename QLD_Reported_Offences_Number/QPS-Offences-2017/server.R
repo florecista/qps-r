d05be6a29e5a018b07c367b491cbd514ef48bfd6
matthewryan <matthew.ryan@engaccess.com>
2018-02-10 20:56:30 +1000

    Added 2017 Summary to Shiny project

--- QLD_Reported_Offences_Number/QPS-Offences-2017/server.R
+++ QLD_Reported_Offences_Number/QPS-Offences-2017/server.R
@@ -49,6 +49,76 @@ person_chart$Crime <- factor(person_chart$Crime, levels = c("Homicide", "Assault
 # Creating 2017 dataset
 person_chart_2017 <- filter(person_chart, Year == "2017")
 
+# OFFENDERS BY SEX
+
+# Loading file 
+data_gender <- read_csv("QLD_Reported_Offenders_Number.csv")
+
+# Selecting columns of interest
+gender_all <- select(data_gender, `Month Year`:`Other Homicide`, Assault, 
+                     `Sexual Offences`, Robbery, `Other Offences Against the Person`,
+                     `Unlawful Entry`, Arson, `Other Property Damage`, 
+                     `Unlawful Use of Motor Vehicle`, 
+                     `Other Theft (excl. Unlawful Entry)`, Fraud, 
+                     `Handling Stolen Goods`, `Drug Offences`, 
+                     `Prostitution Offences`, 
+                     `Liquor (excl. Drunkenness)`: `Weapons Act Offences`, 
+                     `Good Order Offences`, `Stock Related Offences`, 
+                     `Traffic and Related Offences`, `Miscellaneous Offences`)
+
+# Preparing dataset
+gender_all <- mutate(gender_all, Homicide = `Homicide (Murder)` + `Other Homicide`)
+gender_all <- rename(gender_all, Date = `Month Year`)
+
+# Creating Year column
+gender_all$Year <- substr(gender_all$Date, 4, 5)
+gender_all$Help <- rep(20, 1224)
+gender_all$Year <- paste(gender_all$Help, gender_all$Year, sep = "")
+
+# Dividing dataset into "adult" and "juvenile"
+gender_all <- select(gender_all, Year, Age, Sex, Homicide, Assault:`Miscellaneous Offences`)
+gender_adult <- filter(gender_all, Age == "Adult")
+gender_juvenile <- filter(gender_all, Age == "Juvenile")
+
+# Adult dataset
+gender_adult_sex <- gender_adult %>%
+  gather(Crime, Number, Homicide:`Miscellaneous Offences`) %>%
+  group_by(Year, Crime, Sex) %>%
+  summarise(Number = sum(Number))
+
+gender_adult_chart <- gender_adult_sex %>%
+  group_by(Year, Crime) %>%
+  summarise(Total = sum(Number)) %>%
+  right_join(gender_adult_sex, by = c("Year", "Crime")) %>%
+  mutate(percentage = Number/Total*100, 
+         label = paste0(round(percentage), "%")) %>%
+  filter(Year != "2001") %>%
+  filter(Sex != "Not Stated")
+
+# Plotting 2017 adult dataset
+gender_adult_chart_2017 <- gender_adult_chart %>%
+  filter(Year == "2017") %>%
+  ungroup(Year)
+
+# Juvenile dataset
+gender_juvenile_sex <- gender_juvenile %>%
+  gather(Crime, Number, Homicide:`Miscellaneous Offences`) %>%
+  group_by(Year, Crime, Sex) %>%
+  summarise(Number = sum(Number))
+
+gender_juvenile_chart <- gender_juvenile_sex %>%
+  group_by(Year, Crime) %>%
+  summarise(Total = sum(Number)) %>%
+  right_join(gender_juvenile_sex, by = c("Year", "Crime")) %>%
+  mutate(percentage = Number/Total*100, 
+         label = paste0(round(percentage), "%")) %>%
+  filter(Year != "2001") %>%
+  filter(Sex != "Not Stated")
+
+gender_juvenile_chart_2017 <- gender_juvenile_chart %>%
+  filter(Year == "2017") %>%
+  ungroup(Year)
+
 shinyServer(function(input,output,session){
   vals<-reactiveValues()
   vals$collapsed=FALSE
@@ -117,6 +187,38 @@ shinyServer(function(input,output,session){
       theme(legend.position = "bottom", legend.text = element_text(size = 7),
             title = element_text(size = 14),
             legend.title = element_text(size = 10)) +
+      scale_fill_brewer(palette = "Set1")
+  })
+  
+  output$adultplot <- renderPlot({
+    # Plotting adult dataset
+    ggplot(data = gender_adult_chart_2017, 
+           aes(x = Crime, 
+               y = Number, 
+               fill = Sex,
+               label = label)) +
+      geom_col(position = "fill") + 
+      coord_flip() +
+      geom_text(size = 3, col = "black", position = position_fill(vjust = 0.5)) +
+      labs(title = "Offenders of Crime by Sex, 2017", subtitle = "Adults", y = "Share") +
+      theme(axis.text.x = element_blank(), axis.text.y = element_text(size = 6),
+            title = element_text(size = 14)) +
+      scale_fill_brewer(palette = "Set1")
+  })
+  
+  output$juvenileplot <- renderPlot({
+    # Plotting juvenile dataset
+    ggplot(data = gender_juvenile_chart_2017, 
+           aes(x = Crime, 
+               y = Number, 
+               fill = Sex,
+               label = label)) +
+      geom_col(position = "fill") + 
+      coord_flip() +
+      geom_text(size = 3, col = "black", position = position_fill(vjust = 0.5)) +
+      labs(title = "Offenders of Crime by Sex, 2017", subtitle = "Juveniles", y = "Share") +
+      theme(axis.text.x = element_blank(), axis.text.y = element_text(size = 6),
+            title = element_text(size = 14)) +
       scale_fill_brewer(palette = "Set1")
   })
 })
--- QLD_Reported_Offences_Number/QPS-Offences-2017/ui.R
+++ QLD_Reported_Offences_Number/QPS-Offences-2017/ui.R
@@ -3,26 +3,40 @@ library(shinydashboard)
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Charts", icon = icon("bar-chart-o"), startExpanded = TRUE,
-             menuSubItem("Pie", tabName = "pieplot", selected = TRUE),
-             menuSubItem("Filled", tabName = "filledplot"),
-             menuSubItem("Stacked", tabName = "stackedplot")
+    menuItem("Offences", startExpanded = TRUE,
+      menuItem("Charts", startExpanded = TRUE, icon = icon("bar-chart-o"), tabName="charts",
+         menuSubItem("Filled", tabName = "filledplot", selected = TRUE),
+         menuSubItem("Stacked", tabName = "stackedplot")
+      )
+    ),
+    menuItem("2017 Summary", startExpanded = FALSE,
+             menuItem("Charts", icon = icon("bar-chart-o"), tabName="charts",
+                      menuSubItem("Offences", tabName = "pieplot"),
+                      menuSubItem("Adults", tabName = "adultplot"),
+                      menuSubItem("Juveniles", tabName = "juvenileplot")
+             )
     )
   )
 )
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "pieplot",
-            plotOutput("pieplot")
-    ),
-    
     tabItem(tabName = "filledplot",
             plotOutput("filledplot")
     ),
     
     tabItem(tabName = "stackedplot",
             plotOutput("stackedplot")
+    ),
+    tabItem(tabName = "pieplot",
+            plotOutput("pieplot")
+    ),
+    
+    tabItem(tabName = "adultplot",
+            plotOutput("adultplot")
+    ),
+    tabItem(tabName = "juvenileplot",
+            plotOutput("juvenileplot")
     )
   )
 )
